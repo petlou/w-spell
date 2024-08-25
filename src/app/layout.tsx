90263b8f8b09f7328b2a6d9114c0f9cdebf5943e
Peter Lourenço <lourenco.pet@gmail.com>
2024-08-24 23:52:49 -0300

    Refactor: Reestruturação de pastas e arquivos base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import { Roboto } from "next/font/google"
-import "./globals.css"
+import "../styles/globals.css"
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["300", "500", "700"] })
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,1 @@
-import Image from "next/image"
-
-import spellbook from "@assets/spellbook.png"
-
-export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center p-8 bg-zinc-900 text-slate-100">
-			<div className="flex items-center justify-center gap-6">
-				<Image src={spellbook} alt="Imagem de livro de magias" width={40} />
-				<h1 className="text-2xl">Wizard SpellBook</h1>
-			</div>
-		</main>
-	)
-}
+export { default } from "@app/home"
